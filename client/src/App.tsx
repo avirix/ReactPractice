cdc8cb508d6bb4b868f92f6401c1f52db278fbe4
Alexander <alexandersviridenko98@gmail.com>
2019-09-10 15:58:45 +0300

    add base components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
+import Home from './components/Home';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">Simple app (React + .NET Core)</a>
+        <Header />
       </header>
+      <Home />
     </div>
   );
 }
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header: React.FC = () => {
+    return (
+        <div className="collapse navbar-collapse" >
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item"><Link className="nav-link" to='/'>Home</Link></li>
+                <li className="nav-item"><Link className="nav-link" to='/login'>Login</Link></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
