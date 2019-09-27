bfc583085361e61574bfae21f6d7473c29e23885
Alexander <alexandersviridenko98@gmail.com>
2019-09-27 19:03:38 +0300

    add icon

--- client/src/components/timepicker/TimePickerClock.tsx
+++ client/src/components/timepicker/TimePickerClock.tsx
@@ -9,10 +9,16 @@ export interface ClockProps {
 class TimePickerClock extends React.Component<ClockProps> {
     render() {
         return (
-            <div className="mb-2">
+            <div className="mb-2 input-group">
                 <input type="time" className="form-control"
                     value={this.props.hours + ":" + this.props.minute}
                     onChange={(e) => this.props.onChange(e.target.value)} />
+                <div className="input-group-prepend">
+                    <span className="input-group-text">
+                        <img src="/clock.png" className="img-responsive" width="24" height="24"></img>
+                    </span>
+                </div>
+                
             </div>
         );
     }
